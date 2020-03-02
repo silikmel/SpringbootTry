e714a7a6b355233e055e1eebbfc62915b13be7ac
Zhang, Yao <crrc_zy@aliyun.com>
2020-03-02 11:55:31 +0800

    first commit

---
+++ src/main/java/com/cccar/rd/cnr_csdb/CnrCsdbApplication.java
@@ -0,0 +1,13 @@
+package com.cccar.rd.cnr_csdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnrCsdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnrCsdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cccar/rd/cnr_csdb/CnrCsdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cccar.rd.cnr_csdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnrCsdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
